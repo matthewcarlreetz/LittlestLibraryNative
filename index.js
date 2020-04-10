6ea6a599e3a6ed6d87d8b5ce48d35ee183758e2f
Matthew Reetz <matt@headway.io>
2020-04-10 11:44:13 -0500

    Integrate react native navigation

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import HomeScreen from './components/home/HomeScreen.tsx';
 
-import App from './App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('Home', () => HomeScreen);
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Home',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
