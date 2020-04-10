7bccd109190f6e6a11d03357c1f777f98cc66df5
Matthew Reetz <matt@headway.io>
2020-04-10 15:17:11 -0500

    Pull libraries into map/list views

--- components/home/LibraryList.tsx
+++ components/home/LibraryList.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
 import { useNavigationButtonPress } from 'react-native-navigation-hooks';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_LIBRARIES, LibraryData, LibraryVars } from '../../models/library';
 
 const LibraryList = (): JSX.Element => {
+  const { data: { nearbyLibraries } = { nearbyLibraries: [] } } = useQuery<LibraryData, LibraryVars>(GET_LIBRARIES, {
+    variables: { latitude: 44.4, longitude: -88.2 },
+  });
+
   useNavigationButtonPress((e) => {
     console.log(`Pressed ${e.buttonId} on componentId: ${e.componentId}`);
   });
+
   return (
     <SafeAreaView>
-      <Text>ListView</Text>
+      <Text>List</Text>
+      {nearbyLibraries.map((l) => {
+        return <Text key={l.id}>{l.address}</Text>;
+      })}
     </SafeAreaView>
   );
 };
