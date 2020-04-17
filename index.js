141ad406d30cfab295494780a95092d34d652f3f
Matthew Reetz <matt@headway.io>
2020-04-17 14:54:47 -0500

    Add paper. Create basic listview

--- index.js
+++ index.js
@@ -9,15 +9,31 @@ import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const client = new ApolloClient({
   link: createHttpLink({ uri: 'http://localhost:4000/graph' }),
   cache: new InMemoryCache(),
 });
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgb(91,196,180)',
+    secondary: 'yellow',
+    accent: 'yellow',
+  },
+};
+
 const asdf = (Component) => () => (props) => (
   <ApolloProvider client={client}>
-    <Component {...props} />
+    <SafeAreaProvider>
+      <PaperProvider theme={theme}>
+        <Component {...props} />
+      </PaperProvider>
+    </SafeAreaProvider>
   </ApolloProvider>
 );
 
