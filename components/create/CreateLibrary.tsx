a7d032851f700e905d3efba960422d67aec4292f
Matthew Reetz <matt@headway.io>
2020-05-01 08:25:36 -0500

    Fetch user coordinates after taking library picture

--- components/create/CreateLibrary.tsx
+++ components/create/CreateLibrary.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Dimensions, Image as ImageView } from 'react-native';
 import { Button } from 'react-native-paper';
 import { Image } from 'react-native-image-crop-picker';
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_LIBRARY, Library } from '../../models/library';
 import { ReactNativeFile } from 'apollo-absinthe-upload-link';
+import Geolocation, { GeoCoordinates } from 'react-native-geolocation-service';
 
 const styles = StyleSheet.create({
   container: {
@@ -33,9 +34,32 @@ const CreateLibrary = ({ image }: CreateLibraryProps): JSX.Element => {
     { file: string; latitude: number; longitude: number }
   >(CREATE_LIBRARY);
 
+  const [coords, setCoords] = useState<GeoCoordinates | null>(null);
+
   console.log({ error, data, loading });
 
   const win = Dimensions.get('window');
+
+  useEffect(() => {
+    //reverse geocode
+    console.log('coords changed', coords);
+  }, [coords]);
+
+  useEffect(() => {
+    Geolocation.watchPosition(
+      (position) => {
+        console.log('set coords to ', position.coords);
+        setCoords(position.coords);
+      },
+      (error) => {
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    );
+    return (): void => {
+      Geolocation.stopObserving();
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
